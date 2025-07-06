ca8489f41dea761524203229a025f3d5f395696a
Mainul Islam <mainulislam6168@gmail.com>
2025-07-06 23:02:01 +0600

    variables and concept of getting value from command line

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,7 @@ component=shipping
 MAVEN
 
 dnf install mysql -y
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/schema.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/app-user.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/master-data.sql
+# password is: RoboShop@1
