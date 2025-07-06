fe79783fc1d44d47cee7bd710dab9ddce8cba126
Mainul Islam <mainulislam6168@gmail.com>
2025-07-06 17:28:20 +0600

    making code DRY

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,8 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+source common.sh
+component=catalogue
+NODEJS
 
-cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.maidevops.fun </app/db/master-data.js
-
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
