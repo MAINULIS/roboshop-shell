092a0ec4ab09883914b4530657ba82775a09b255
Mainul Islam <mainulislam6168@gmail.com>
2025-07-08 07:00:19 +0600

    testing function variable declared outside

--- dispatch.sh
+++ dispatch.sh
@@ -1,17 +1,10 @@
 dnf install golang -y
 
-cp dispatch.service /etc/systemd/system/dispatch.service
+source common.sh
+component=dispatch
+APP_PREREQ
 
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
-cd /app
-unzip /tmp/dispatch.zip
-
-cd /app
 go mod init dispatch
 go get
 go build
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+SYSTEMD
